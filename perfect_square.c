3f81fb3a9e62c8ed191f21213a4d60a4225a2794
Jashu Bhardwaj <74259821+JashuBhardwaj@users.noreply.github.com>
2021-07-22 12:32:13 +0530

    Update perfect_square.c

--- perfect_square.c
+++ perfect_square.c
@@ -1,9 +1,21 @@
+// program to find square root of an no with precision using binary search
+//Author name - Jashu Bhardwaj
+// Roll No - 1910991028
+// Date : 20/07/2021
+
+
 #include <stdio.h>
+
+// Function to find square root and return result
+// it takes the no and the precision as arguments
 
 float perfect_square(int no,int precision){
     
     int left = 0,right = no,mid = 0;
-    float res;
+    float res=0;
+
+    // calculating the int part of the result using binary search
+
     while(left <= right){
         mid = ( left + right )/2;
         if( mid * mid == no) {
@@ -21,6 +33,9 @@ float perfect_square(int no,int precision){
     }
     
     float prec=0.1;
+
+    // finding the precision of the no 
+
     for(int i=0;i<precision;i++) {
         while(res*res<=no) {
             res+=prec;
@@ -33,12 +48,18 @@ float perfect_square(int no,int precision){
 }
 
 int main() {
+
     int no=0;
     int precision=0;
+
+    //scanning the no and the precision from user 
+
     printf("Enter a no: ");
     scanf("%d",&no);
     printf("Enter a precision: ");
-    scanf("%d",&precision);s
+    scanf("%d",&precision);
+
+    // calling the perfect_square function to calculate result
 
     float result= perfect_square(no, precision);
     
